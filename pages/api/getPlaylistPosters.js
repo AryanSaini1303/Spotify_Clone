1c47e6b2413a506e4f80913fb1b494a1c06382eb
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-06-04 21:16:15 +0530

    resolved the issue of changing order of playlist posters(for loop instead of promise.all)

--- pages/api/getPlaylistPosters.js
+++ pages/api/getPlaylistPosters.js
@@ -6,17 +6,27 @@ export default async function getPlaylistPosters(req,res) {
   const playlists = await getPlaylists();
 
   // Use Promise.all to await all async operations
-  await Promise.all(playlists.map(async (playlist) => {
+  // await Promise.all(playlists.map(async (playlist) => {
+  //   const response = await prisma.playlistSong.findFirst({
+  //     where: { playlistId: playlist.id },
+  //     select: { song: { select: { poster: true } } }, // Select only the 'poster' field from the related song
+  //   });
+  //   // If a matching poster is found, push it into the posters array
+  //   if (response) {
+  //     posters.push(response.song.poster);
+  //   }
+  // }));
+  // using above method was fine but resulted in changing the order of response every once in a while so i used for loop
+  
+  for (let i = 0; i < playlists.length; i++) {
     const response = await prisma.playlistSong.findFirst({
-      where: { playlistId: playlist.id },
+      where: { playlistId: playlists[i].id },
       select: { song: { select: { poster: true } } }, // Select only the 'poster' field from the related song
     });
-
-    // If a matching poster is found, push it into the posters array
     if (response) {
       posters.push(response.song.poster);
     }
-  }));
+  }
 
   res.status(200).json(posters);
 }
