10262fbfcd17c5cd2018f4db6f68c0068657420b
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-06-05 14:26:30 +0530

    last commit was buggy: reverting the last commit

--- components/content.js
+++ components/content.js
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 import PlayerSection from "@/components/playerSection";
 import MainSection from "@/components/mainSection";
 import InfoSection from "@/components/infoSection";
@@ -9,53 +9,40 @@ import styles from "./content.module.css";
 import { usePathname } from "next/navigation";
 import PlaylistSection from "./playlistSection";
 
-export default function Content({ id }) {
-  const currentRoute = usePathname();
-  const [minLib, setMinLib] = useState(() => {
-    // Retrieve the initial state from local storage
-    if (typeof window !== 'undefined') {
-      return JSON.parse(localStorage.getItem('minLib')) || false;
-    }
-    return false;
-  });
-
-  useEffect(() => {
-    // Update local storage whenever minLib changes
-    localStorage.setItem('minLib', JSON.stringify(minLib));
-  }, [minLib]);
+export default function Content({id}) {
+  const currentRoute=usePathname();
+  // console.log(currentRoute.match("/playlist"));
+  const [minLib, setMinLib] = useState();
 
   function handleMinLib() {
-    setMinLib(prevMinLib => !prevMinLib);
+    setMinLib(!minLib);
   }
-
-  const [playlistPosters, setPlaylistsPosters] = useState([]);
+  console.log(minLib);
   
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/getPlaylistPosters");
-        if (!response.ok) {
+  const [playlistPosters,setPlaylistsPosters]=useState([])
+  useEffect(()=>{
+    const fetchData=async()=>{
+      try{
+        const response=await fetch("/api/getPlaylistPosters");
+        if(!response.ok){
           throw new Error(`Error fetching data: ${response.statusText}`);
         }
-        const data = await response.json();
+        const data=await response.json();
         setPlaylistsPosters(data);
-      } catch (err) {
-        console.error("Error fetching playlists:", err);
+      }catch(err){
+        console.error("Error fetching playlists:",err);
       }
     };
     fetchData();
-  }, []);
+  },[]);
 
   return (
     <div className={styles.mainPageContainer}>
       <NavSection className={minLib ? styles.minNavSection : styles.navSection} minimize={minLib} />
-      <LibSection className={!minLib ? styles.libSection : styles.minLibSection} onClick={handleMinLib} minimize={minLib} playlistPosters={playlistPosters} />
+      <LibSection className={!minLib ? styles.libSection : styles.minLibSection} onClick={handleMinLib} minimize={minLib} playlistPosters={playlistPosters}/>
       <PlayerSection className={styles.playerSection} />
       <InfoSection className={!minLib ? styles.infoSection : styles.minInfoSection} />
-      {!currentRoute.startsWith("/playlist") ? 
-        <MainSection className={!minLib ? styles.mainSection : styles.minMainSection} playlistPosters={playlistPosters} /> : 
-        <PlaylistSection className={!minLib ? styles.mainSection : styles.minMainSection} id={id} />
-      }
+      {!currentRoute.startsWith("/playlist")?<MainSection className={!minLib ? styles.mainSection : styles.minMainSection} playlistPosters={playlistPosters}/>:<PlaylistSection className={!minLib ? styles.mainSection : styles.minMainSection} id={id}/>}
     </div>
   );
 }
