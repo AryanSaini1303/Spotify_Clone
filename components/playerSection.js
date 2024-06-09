b6c58657a53e911c503ea0497043c29ff4ec3642
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-06-09 16:17:22 +0530

    added song info structure in the player section

--- components/playerSection.js
+++ components/playerSection.js
@@ -1,12 +1,21 @@
+import styles from "./playerSection.module.css"
 export default function PlayerSection({ className }) {
-    return (
-      <>
-        <section className={className}>
-          <div className="song"></div>
-          <div className="player"></div>
-          <div className="navs"></div>
-        </section>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <section className={className}>
+        <div className={styles.songCard}>
+          <img src='/demoImage.jpeg' alt="" />
+          <div className={styles.songDetails}>
+            <h5>Meditate me</h5>
+            <h6>Ekoh, Nate Vickers</h6>
+          </div>
+          <div className={styles.add}>
+            <img src="/add.svg" alt="" />
+          </div>
+        </div>
+        <div className="player"></div>
+        <div className="navs"></div>
+      </section>
+    </>
+  );
+}
