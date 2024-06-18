c27b2190f0b98a554ae67aacd6dc3b2a308e2922
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-06-19 01:09:22 +0530

    save

--- components/playerSection.js
+++ components/playerSection.js
@@ -1,10 +1,11 @@
-import styles from "./playerSection.module.css"
+import Image from "next/image";
+import styles from "./playerSection.module.css";
 export default function PlayerSection({ className }) {
   return (
     <>
       <section className={className}>
-        <div className={styles.songCard}>
-          <img src='/demoImage.jpeg' alt="" />
+        <section className={styles.songCard}>
+          <img src="/demoImage.jpeg" alt="" />
           <div className={styles.songDetails}>
             <h5>Meditate me</h5>
             <h6>Ekoh, Nate Vickers</h6>
@@ -12,8 +13,46 @@ export default function PlayerSection({ className }) {
           <div className={styles.add}>
             <img src="/add.svg" alt="" />
           </div>
-        </div>
-        <div className="player"></div>
+        </section>
+        <section className={styles.player}>
+          <div className={styles.btns}>
+            <div className={`${styles.previousBtn} ${styles.nexprev}`}>
+              <Image
+                src="/leftNext.svg"
+                alt="previous"
+                width={22}
+                height={22}
+                style={{ filter: "invert(100%)" }}
+              />
+            </div>
+            <div className={styles.playBtn}>
+              <Image
+                src="/playButtonSharp.svg"
+                alt="play"
+                width={22}
+                height={22}
+              />
+            </div>
+            <div className={`${styles.nextBtn} ${styles.nexprev}`}>
+              <Image
+                src="/rightNext.svg"
+                alt="next"
+                width={22}
+                height={22}
+                style={{ filter: "invert(100%)" }}
+              />
+            </div>
+            <div className={`${styles.repeatBtn} ${styles.nexprev}`}>
+              <Image
+                src="/repeat.svg"
+                alt="repeat"
+                width={22}
+                height={22}
+                style={{ filter: "invert(100%)" }}
+              />
+            </div>
+          </div>
+        </section>
         <div className="navs"></div>
       </section>
     </>
