bfd5c3f195d9be79e528a15d9695b2ceb38efbfe
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-06-27 05:14:46 +0530

    added a working seek bar in the player section with play/pause and traversing functionality

--- components/playerSection.js
+++ components/playerSection.js
@@ -1,6 +1,91 @@
 import Image from "next/image";
 import styles from "./playerSection.module.css";
+import { useRef, useState, useEffect } from "react";
+
 export default function PlayerSection({ className }) {
+  const progressRef = useRef(null);
+  const playPauseRef = useRef(null);
+  const songRef = useRef(null);
+  const [play, setPlay] = useState(false);
+  const [progressBarValue,setProgressBarValue]=useState(0);
+  const [progressMouseEnter,setProgressMouseEnter]=useState(false);
+  const [progressMouseLeave,setProgressMouseLeave]=useState(false);
+  const [songDuration, setSongDuration] = useState(0);
+  const [songCurrentTime, setSongCurrentTime] = useState(0);
+  useEffect(()=>{
+    if (songRef.current) {
+      setSongDuration(`${Math.floor(songRef.current.duration/60)}:${Math.round(songRef.current.duration%60)}`)
+      progressRef.current.max = songRef.current.duration;
+      progressRef.current.value = 0;  
+    }
+  },[])
+  
+  function updateProgressBar() {
+    if (songRef.current && progressRef.current) {
+      progressRef.current.value = songRef.current.currentTime;
+      setSongCurrentTime(`${Math.floor(songRef.current.currentTime/60)}:${Math.round(songRef.current.currentTime%60)<10?"0":""}${Math.round(songRef.current.currentTime%60)}`);
+      setProgressBarValue((songRef.current.currentTime/songRef.current.duration)*100);
+    }
+  }
+
+  function resetProgressBar() {
+    if (progressRef.current) {
+      progressRef.current.value = 0;
+      setPlay(!play);
+      setProgressBarValue(0);
+      songRef.current.currentTime=0;
+    }
+  }
+
+  function updateSongProgress() {
+    if (songRef.current) {
+      songRef.current.currentTime = progressRef.current.value;
+      if (!play) {
+        setPlay((prevPlay) => {
+          const newPlay = !prevPlay;
+          if (newPlay) {
+            songRef.current.play();
+          } else {
+            songRef.current.pause();
+          }
+          return newPlay;
+        });
+      }
+    }
+  }
+
+  function handlePlayClick() {
+    setPlay((prevPlay) => {
+      const newPlay = !prevPlay;
+      if (newPlay) {
+        songRef.current.play();
+      } else {
+        songRef.current.pause();
+      }
+      return newPlay;
+    });
+  }
+
+  function handleProgressMouseEnter(){
+    setProgressMouseEnter(!progressMouseEnter);
+    setProgressMouseLeave(false);
+  }
+  function handleProgressMouseLeave(){
+    setProgressMouseEnter(false);
+    setProgressMouseLeave(!progressMouseLeave);
+  }
+  
+  let backgroundColor;
+  if(progressMouseEnter){
+    backgroundColor='#1FDF64'
+  }
+  if(progressMouseLeave){
+    backgroundColor='white';
+  }
+  const overlayStyle = {
+    width: `${progressBarValue}%`,
+    backgroundColor
+  };
   return (
     <>
       <section className={className}>
@@ -15,6 +100,13 @@ export default function PlayerSection({ className }) {
           </div>
         </section>
         <section className={styles.player}>
+          <audio
+            ref={songRef}
+            onTimeUpdate={updateProgressBar}
+            onEnded={resetProgressBar}
+          >
+            <source src="/songs/onMyWay.mp3" type="audio/mpeg" />
+          </audio>
           <div className={styles.btns}>
             <div className={`${styles.previousBtn} ${styles.nexprev}`}>
               <Image
@@ -25,12 +117,17 @@ export default function PlayerSection({ className }) {
                 style={{ filter: "invert(100%)" }}
               />
             </div>
-            <div className={styles.playBtn}>
+            <div
+              className={styles.playBtn}
+              ref={playPauseRef}
+              onClick={handlePlayClick}
+            >
               <Image
-                src="/playButtonSharp.svg"
+                src={!play ? "/playButtonSharp.svg" : "/pause.svg"}
                 alt="play"
-                width={21}
-                height={21}
+                width={!play ? 21 : 15}
+                height={!play ? 21 : 15}
+                style={!play ? { paddingLeft: "0.15rem" } : null}
               />
             </div>
             <div className={`${styles.nextBtn} ${styles.nexprev}`}>
@@ -42,18 +139,23 @@ export default function PlayerSection({ className }) {
                 style={{ filter: "invert(100%)" }}
               />
             </div>
-            <div className={`${styles.repeatBtn} ${styles.nexprev}`}>
-              <Image
-                src="/repeat.svg"
-                alt="repeat"
-                width={22}
-                height={22}
-                style={{ filter: "invert(100%)" }}
-              />
+            <div className={styles.progressBarContainer}>
+              <h6 className={styles.currentTime}>{songCurrentTime}</h6>
+              <div className={styles.overlay} style={overlayStyle}></div>
+              <input
+                onMouseEnter={handleProgressMouseEnter}
+                onMouseLeave={handleProgressMouseLeave}
+                type="range"
+                name="progressBar"
+                className={styles.progressBar}
+                ref={progressRef}
+                onChange={updateSongProgress}
+                />
+                <h6 className={styles.duration}>{songDuration}</h6>
             </div>
           </div>
         </section>
-        <div className="navs"></div>
+        <div className="otherNavs"></div>
       </section>
     </>
   );
