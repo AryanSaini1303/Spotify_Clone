25cdcb192c2ea589ede89ba7c674e4203d3e108f
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-07-11 14:33:42 +0530

    feat: search section UI started

--- components/searchSection.js
+++ components/searchSection.js
@@ -1,6 +1,46 @@
-import styles from './searchSection.module.css'
-export default function SearchSection(){
-    return(
-        <h1 className={styles.text}>Search Section</h1>
-    )
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import styles from "./searchSection.module.css";
+export default function SearchSection() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  function handleChange(e){
+    setQuery(e.target.value)
+  }
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (query) {
+        handleSearch();
+      }
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [query]);
+  const handleSearch = async () => {
+    const res = await fetch(`/api/getSearchedSongs?query=${query}`);
+    const data = await res.json();
+    setResults(data);
+    console.log(results);
+  };
+  return (
+    <section className={styles.searchContainer}>
+      <header className={styles.searchComponent}>
+        <Image
+          src="/search.svg"
+          width={17}
+          height={17}
+          style={{ filter: "invert(100%)" }}
+          className={styles.searchImg}
+        />
+        <input
+          type="text"
+          id={styles.songInput}
+          placeholder="What do you want to play?"
+          tabIndex={0}
+          onChange={handleChange}
+        />
+      </header>
+    </section>
+  );
 }
